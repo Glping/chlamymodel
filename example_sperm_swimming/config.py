7de8d70cd388608823abcafcc98638605c1a0a72
garykl <spiehr@gmx.de>
2017-04-26 11:29:43 +0200

    .

---
+++ example_sperm_swimming/config.py
@@ -0,0 +1,13 @@
+import numpy as np
+# pylint: disable=E1101
+
+processes = 7
+timestep = 0
+origin = (0, 0, 0)
+rotation = np.diag([1, 1, 1])
+spatialResolution = 80
+headGrid = 40
+length = 60
+timeResolution = 100
+timestepsH = 100
+frequency = 0.05
